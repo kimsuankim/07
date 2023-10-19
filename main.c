e3a45ddcb17f0a95fdca9de4bc8e981479da8569
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2023-10-19 15:44:54 +0900

    3

--- main.c
+++ main.c
@@ -3,16 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter);
 int main(int argc, char *argv[]) {
 	
-	int i;
-	
-	for(i=0; i<5; i++)
-	{
-		static int temp = 1;
-		printf("temp = %d\n", temp);
-		temp++;
-	}
+	int i=10;
+	printf("함수호출전i= %d\n", i);
+	i= inc(i); //변수=함수결과(함수에 의해 리턴된값) 
+	printf("함수호출후 i= %d\n", i);
 	
 	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
