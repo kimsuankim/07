d9107c5f94a88fbc982f177ab403ae19e818638f
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2023-10-19 16:24:58 +0900

    6-1

--- main.c
+++ main.c
@@ -3,22 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void sub(void);
+int all_files;
+
+extern void sub();
+ 
 int main(int argc, char *argv[]) {
-	int i; 
-	for(i=0; i<3; i++)
-	 sub();
+	sub();
+	printf("%d\n", all_files);
 	return 0;
 }
 
-void sub(void)
- {
- 	int auto_count= 0; //stack에 0저장 
-	static int static_count= 0;
-	auto_count++; //stack 에 저장 
-	static_count++; //data 에 저장 
-	printf("auto_count=%d\n", auto_count); 
-	printf("static_count=%d\n", static_count); 
-	//stack 끝 
- }
 
